646f3a86d77e8daf3223e64c017be17b68e7bbac
Andrea Zarbo <andreaz@mirageoscience.com>
2014-03-25 11:14:32 -0400

    Initial commit

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+run();
+
+/**
+*   @brief run
+*
+*       Main function, prints a message.
+*
+*   @author Andrea Zarbo Mira Geoscience Ltd
+*   @date  2014-03-24
+*
+**/
+function run(){
+    $name = "quandrei";
+    echo "Hello $name, this is a git-errific tutorial\n";
+}//END function run
